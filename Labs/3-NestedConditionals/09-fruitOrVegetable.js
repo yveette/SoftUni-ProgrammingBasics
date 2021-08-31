a1ad4ab7626d44fccd5c92dfc17642a656551075
yveette <89652745+yveette@users.noreply.github.com>
2021-08-31 14:38:02 +0300

    Add files via upload

---
+++ Labs/3-NestedConditionals/09-fruitOrVegetable.js
@@ -0,0 +1,16 @@
+function whatIsIt(input) {
+    let what = input[0];
+
+    if (what == "banana" || what == "apple" || what == "kiwi" || what == "cherry" || what == "lemon" || what == "grapes") {
+        console.log("fruit");
+    } else if (what == "tomato" || what == "cucumber" || what == "pepper" || what == "carrot") {
+        console.log("vegetable");
+    } else {
+        console.log("unknown");
+    }
+}
+
+whatIsIt(["banana"]);
+whatIsIt(["apple"]);
+whatIsIt(["tomato"]);
+whatIsIt(["water"]);
